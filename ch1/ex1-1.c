6ccd9d3e255bad0349bf407b17065afba9a6b12c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-01-25 18:13:42 +0900

    add ch1

---
+++ ch1/ex1-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    // printf("Hello, world\n");
+    // printf("Bye!\n");
+    printf("Hello, world\nBye!\n");
+    return 0;
+}
---
+++ ch1/ex1-1_adv.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("To be, or not to be:\n");
+    printf("        that is the question.\n");
+    printf("                        -- Hamlet\n");
+    return 0;
+}
