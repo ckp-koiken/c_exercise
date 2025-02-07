81485555840b4a885d569703c865d2633b7e5ee3
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-07 12:57:41 +0900

    add check_num

--- ch4/ex4-3_adv.c
+++ ch4/ex4-3_adv.c
@@ -9,10 +9,11 @@ int check_num(int card);
 
 int card_draw(void) {
     int card;
-    card = (rand() % 4 + 1) * 100 + (rand() % 10 + 2);
+    card = (rand() % 4 + 1) * 100 + (rand() % 13 + 1);
     return card;
 }
 
+// TODO: 柄の絵文字を表示できるようにする
 char check_pattern(int card) {
     int check;
     check = card / 100;
@@ -29,7 +30,14 @@ char check_pattern(int card) {
 }
 
 int check_num(int card) {
-        return card % 10;
+    int check;
+    check = card % 100;
+    if (check == 1)
+        return 11;
+    else if (check >= 11)
+        return 10;
+    else 
+        return check;
 }
 
 void wait_enter(void) {
@@ -39,10 +47,12 @@ void wait_enter(void) {
 }
 
 int main(void) {
-    int c_card1, c_card2, c_cards_total, y_card1, y_card2, y_cards_total;
-    int c_card1_pat, c_card2_pat, c_card1_num, c_card2_num;
-    int y_card1_pat, y_card2_pat, y_card1_num, y_card2_num;
+    int c_card1, c_card2, c_cards_total, c_card1_num, c_card2_num, y_card1, y_card2, y_cards_total, y_card1_num, y_card2_num;
+    char c_card1_pat, c_card2_pat, y_card1_pat, y_card2_pat; 
+
     srand(time(0));
+
+    // コンピュータの番
     printf("コンピュータ");
     c_card1 = card_draw();
     c_card2 = card_draw();
@@ -51,10 +61,11 @@ int main(void) {
     c_card1_num = check_num(c_card1);
     c_card2_num = check_num(c_card2);
     c_cards_total = c_card1_num + c_card2_num;
-    printf("%c%d %c%d 合計%d点\n", c_card1_pat, c_card1_num, c_card2_pat, c_card2_num, c_cards_total);
+    printf("%c%d %c%d 合計%d点\n", c_card1_pat, c_card1_num, c_card2_pat, c_card2_num, c_cards_total); // TODO: エースをAと表現できるようにする
 
     wait_enter();
 
+    // あなたの番
     y_card1 = card_draw();
     y_card2 = card_draw();
     y_card1_pat = check_pattern(y_card1);
@@ -66,6 +77,7 @@ int main(void) {
     printf("2枚目 %c%d\n", y_card2_pat, y_card2_num);
     printf("合計%d点\n", y_cards_total);
 
+    // 勝敗判定
     if (c_cards_total == y_cards_total)
         printf("引き分け\n");
     else if (c_cards_total > y_cards_total)
