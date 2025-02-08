0e12975ecb7b52a74f39879599ae5500ac67003a
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-08 21:20:52 +0900

    fix compound_i

--- ch4/ex4-4_adv.c
+++ ch4/ex4-4_adv.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-int compound_i(int p, int y, int rp);
+double compound_i(int p, int y, double rp);
 
-// TODO: int型のみで小数の利息の処理を追加
-int compound_i(int p, int y, int rp) {
+// TODO: int型のみでの処理に修正
+double compound_i(int p, int y, double rp) {
     for (int i = 0; i < y; i++)
         p *= (1+rp/100);
     return p;
 }
 
 int main(void) {
-    int pv, year, rate, final;
+    int pv, year;
+    double rate, final;
     
     printf("最初の金額[円]？");
     scanf("%d", &pv);
     printf("複利の年利率[％]？");
-    scanf("%d", &rate);
+    scanf("%lf", &rate);
     printf("年数？");
     scanf("%d", &year);
 
     final = compound_i(pv, year, rate);
 
-    printf("%d年後の金額は%d円です\n", year, final);
+    printf("%d年後の金額は%f円です\n", year, final);
     return 0;
 }
