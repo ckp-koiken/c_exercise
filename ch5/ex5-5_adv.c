75f9c0691d00fb78b0498d887d2854a120701acd
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-13 14:31:35 +0900

    add 5-5_adv

---
+++ ch5/ex5-5_adv.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+enum { N = 100 };
+
+int main(void) {
+    int a[N+1] = { 0 }; // a[N]まで使いたいからN+1を指定
+                        // 0が入っていると素数と判定（合成数にフラグを立てる）
+
+    // Nの約数であるsqrt(N)までの最小の整数倍まで調べればいい
+    for (int i = 2; i < sqrt(N * 1.0); i++) { // 2からsqrt(N)までの整数を順に見る
+        if (a[i] == 0) { // 該当する番手が0だったら(iが素数なら)倍数をふるい落とす
+            for (int j = i * 2; j <= N; j += i) // iの倍数をふるい落とす
+                a[j] = 1;
+        }
+    }
+    
+    // 素数を表示
+    for (int i = 2; i <= N; i++) {
+        if (a[i] == 0)
+            printf("%d ", i);
+    }
+
+    printf("\n");
+    
+    return 0;
+}
