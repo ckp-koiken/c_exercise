f015ef841317cd56898d8ffea728c3e3da0fadb2
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-15 19:34:04 +0900

    add 6-2

---
+++ ch6/ex6-2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void cancel(int *np, int *dp);
+
+void cancel(int *np, int *dp) {
+    int fact = 2;
+    
+    while (fact <= *dp) {
+        if (*np % fact == 0 && *dp % fact == 0) {
+            *np /= fact;
+            *dp /= fact;
+        } else 
+            fact++;
+    }
+}
+
+int main(void) {
+    int n, d;
+
+    printf("分子？");
+    scanf("%d", &n);
+    printf("分母？");
+    scanf("%d", &d);
+    cancel(&n, &d);
+    printf("訳文すると %d/%d\n", n, d);
+    
+    return 0;
+}
