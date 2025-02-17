e6357a6cf9aa020f1001aa7c74741bbc3a6ab51c
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-17 21:47:32 +0900

    add showadj()

--- ch6/ex6-4.c
+++ ch6/ex6-4.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void showadj(float *p);
+
+void showadj(float *p) {
+    if (p[-1] < 0)
+        printf("---------\n");
+    else
+        printf(" %.2f秒\n", p[-1]);
+    
+    printf("* %.2f秒\n", *p);
+
+    if (p[1] < 0)
+        printf("---------\n");
+    else
+        printf(" %.2f秒\n", p[1]);
+}
+
+
 int main(void) {
     float t[] = {-1, 28.89, 29.18, 29.22, 29.66, 30.77, 31.33, -1};
     int i;
@@ -8,6 +25,8 @@ int main(void) {
         printf("%d位 %.2f秒\n", i, t[i]);
     printf("何位の前後？");
     scanf("%d", &i);
+    showadj(&t[i]);
+    // showadj(t+i);
 
     return 0;
 }
