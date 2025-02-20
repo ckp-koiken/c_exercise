291326ca42f7adf887660886b792a34fabd25f6d
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-20 19:49:28 +0900

    add calc_ttime()

--- ch6/ex6-7.c
+++ ch6/ex6-7.c
@@ -2,6 +2,19 @@
 
 #define NELEMS(a) (sizeof(a)/sizeof(a[0]))
 
+void calc_ttime(int *routep[], int ttimep[]);
+
+// 各経路の所要時間を計算する
+void calc_ttime(int *routep[], int ttimep[]) {
+    while (*routep != NULL) {
+        *ttimep = 0;
+        for (int *p = *routep; *p > 0; p++)
+            *ttimep += *p;
+        routep++;
+        ttimep++;
+    }
+}
+
 int main(void) {
     // ルートごとの配列を用意
     int route1[] = {5, 35, 6, -1},
@@ -13,6 +26,9 @@ int main(void) {
     int *routes[] = {route1, route2, route3, route4, route5, NULL};
     int ttime[NELEMS(routes)-1];   // 各経路の所要時間
     int n = NELEMS(ttime);  // 経路数
+
+    calc_ttime(routes, ttime);   // 所要時間を計算してttimeに入れる
+    // show_routes(routes, ttime); // 一覧を表示
 
     return 0;
 }
