3af5d6208d017205bd323c0a3a52d4ec93aad29a
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-28 17:50:26 +0900

    fix

--- ch7/ex7-11.c
+++ ch7/ex7-11.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 char *mgets(void) {
-    char *p;
+    char *p, *newp;
     int c;
     size_t sz = 1;      // 現在の領域サイズを覚えておく変数
 
@@ -11,8 +11,9 @@ char *mgets(void) {
     while ((c = getchar()) != EOF) {    // getcharで文字を受け付ける
         if (c == '\n')              // 改行文字で止める
             break;
-        if ((p = realloc(p, sz+1)) == NULL)  // reallocでpに1文字分追加
-            break;
+        if ((newp = realloc(p, sz+1)) == NULL)  // reallocでnewpに1文字分追加
+            break;      // 拡張した領域の先頭アドレスが以前とは同じとは限らない
+        p = newp;       // バグ対策でいったんnewpに渡す
         sz++;           // 確保サイズを１つ大きくする
         p[sz-2] = c;    // 1番後ろの一つまえに文字を入れる
     }
