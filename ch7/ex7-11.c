dcaeee3bf26e3d5be0818be807e8e6eec5652b33
ckp-koiken <kentaro.koi0710@gmail.com>
2025-02-28 17:40:12 +0900

    fix pointer

--- ch7/ex7-11.c
+++ ch7/ex7-11.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 char *mgets(void) {
-    char *p, *newp;
+    char *p;
     int c;
-    size_t sz = 1;      // 現在の領域を覚えておく変数
+    size_t sz = 1;      // 現在の領域サイズを覚えておく変数
 
-    if ((p = malloc(sz)) == NULL)
+    if ((p = malloc(sz)) == NULL)   // まず1文字分確保
         return NULL;
-    while ((c = getchar()) != EOF) {
-        if (c == '\n')
+    while ((c = getchar()) != EOF) {    // getcharで文字を受け付ける
+        if (c == '\n')              // 改行文字で止める
             break;
-        if ((newp = realloc(p, sz+1)) == NULL)
+        if ((p = realloc(p, sz+1)) == NULL)  // reallocでpに1文字分追加
             break;
-        p = newp;
-        sz++;
-        p[sz-2] = c;
+        sz++;           // 確保サイズを１つ大きくする
+        p[sz-2] = c;    // 1番後ろの一つまえに文字を入れる
     }
-    p[sz-1] = '\0';
+    p[sz-1] = '\0';     // 1番後ろに空文字を入れる
     return p;
 }
 
