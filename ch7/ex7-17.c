a9dc0be18a97ae4da14c7f968808c11499e87a59
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-04 15:19:44 +0900

    add memo

--- ch7/ex7-17.c
+++ ch7/ex7-17.c
@@ -6,8 +6,8 @@ void concat(char *p, char *q) {
     if (*p) {           // pが空文字列でないなら
         while (*p)      // pの末尾を探して下線を追加
             p++;
-        *p++ = '_';
-    }
+        *p++ = '_';     // *p = '_';
+    }                    // p++;
     while (*q)          // qの文字を一文字ずつpに追加する
         *p++ = *q++;
     *p = '\0';          // 空文字で終える
@@ -16,7 +16,7 @@ void concat(char *p, char *q) {
 int main(int argc, char *argv[]) {
     char s[1024] = "";
 
-    while (*++argv)     // argv[1]から順にsに追加
+    while (*++argv)     // argv[1]から順にsに追加(++argv && *argv)
         concat(s, *argv);
     printf("%s\n", s);
     return 0;
