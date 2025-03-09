07fea6c920c7fb8513c45957824ffcb56a356fcf
ckp-koiken <kentaro.koi0710@gmail.com>
2025-03-09 19:49:55 +0900

    add print_skill

--- ch9/ex9-3_adv.c
+++ ch9/ex9-3_adv.c
@@ -11,6 +11,22 @@ struct monster {
     struct skillset condition;
 };
 
+void print_skill(struct monster x);
+
+void print_skill(struct monster x) {
+    printf("スキル: ");
+    if (x.condition.poison == 1) {
+        printf("どく ");
+    }
+    if (x.condition.sleep == 1) {
+        printf("ねむり ");
+    }
+    if (x.condition.paralysis == 1) {
+        printf("マヒ ");
+    }
+    printf("\n");
+}
+
 int main(void) {
     struct monster temp = {"", 7, 4, {0, 0, 0}};
     struct monster m1, m2, m3;
@@ -18,21 +34,19 @@ int main(void) {
     m1 = temp;
     strcpy(m1.name, "giant ant");
     printf("%s: HP = %d MP = %d ", m1.name, m1.hp, m1.mp);
-    printf("スキル: %s %s %s\n", m1.condition.poison == 1 ? "どく" : "", m1.condition.sleep == 1 ? "ねむり" : "", m1.condition.paralysis == 1 ? "マヒ" : "");
-    // TODO: スキルの表示を改善
+    print_skill(m1);
 
     m2 = temp;
     strcpy(m2.name, "floating eye");
     m2.condition.paralysis = 1;
     printf("%s: HP = %d MP = %d ", m2.name, m2.hp, m2.mp);
-    printf("スキル: %s %s %s\n", m2.condition.poison == 1 ? "どく" : "", m2.condition.sleep == 1 ? "ねむり" : "", m2.condition.paralysis == 1 ? "マヒ" : "");
+    print_skill(m2);
 
     m3 = temp;
     strcpy(m3.name, "homunculus");
     m3.condition.poison = 1;
     m3.condition.sleep = 1;
     printf("%s: HP = %d MP = %d ", m3.name, m3.hp, m3.mp);
-    printf("スキル: %s %s %s\n", m3.condition.poison == 1 ? "どく" : "", m3.condition.sleep == 1 ? "ねむり" : "", m3.condition.paralysis == 1 ? "マヒ" : "");
-
+    print_skill(m3);
     return 0;
 }
