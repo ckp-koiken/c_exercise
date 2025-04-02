fad8dbfb43c090cf196b358c64f6dbbce1b207ab
ckp-koiken <kentaro.koi0710@gmail.com>
2025-04-02 15:18:09 +0900

    add memo

--- ch11/ex11-6.c
+++ ch11/ex11-6.c
@@ -4,10 +4,14 @@ void hanoi(int n, char from, char work, char to);
 
 void hanoi(int n, char from, char work, char to) {
     if (n == 1) {
+        // 1枚なら直接目的地(to)へ
+        // ベースケース
         printf("%d: %c -> %c \n", n, from, to);
     } else {
+        // いったん作業場所(work)へ
         hanoi(n - 1, from, to, work);
         printf("%d: %c -> %c\n", n, from, to);
+        // 作業場所(work)から目的地(to)へ
         hanoi(n - 1, work, from, to);
     }
 }
